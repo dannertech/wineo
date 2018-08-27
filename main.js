408490d5362d34009e1aac8ddc2455a72f28dbf6
dannertech <danner@danner.tech>
2018-08-27 01:27:59 -0400

    website files

---
+++ main.js
@@ -0,0 +1,9 @@
+
+var saveEmail = function(){
+var email = document.getElementById('userInput').value;
+console.log(email);
+}
+
+
+var button = document.getElementById("button");
+button.addEventListener("click", saveEmail);
